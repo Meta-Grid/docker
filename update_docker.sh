200c759a0bf52c1cecced70dac2f3bd866565863
patschwork <inetreg@pat-schmitz.de>
2023-05-18 12:25:02 +0200

    Update update_docker.sh

--- update_docker.sh
+++ update_docker.sh
@@ -10,7 +10,7 @@ docker rm meta_grid
 ./build_docker.sh
 
 # use this as update for the database schema
-docker run -i -v $PWD/db:/opt/meta_grid/db -p 8888:80 --name meta_grid_update_db -t meta_grid:latest /bin/bash -c 'cd /opt/meta_grid_updater; python3 meta-grid_install_or_update.py -u -m=update'
+docker run -i -v $PWD/db:/opt/meta_grid/db --name meta_grid_update_db -t meta_grid:latest /bin/bash -c 'cd /opt/meta_grid_updater; python3 meta-grid_install_or_update.py -u -m=update'
 
 # stop and remove running instance
 docker kill meta_grid_update_db
